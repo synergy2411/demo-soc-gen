1cef1a04fd416e0bb57286e2d6330bcc74664ab0
synergy2411 <synergy2411@outlook.com>
2018-07-11 10:52:05 +0530

    Interceptors added

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { USER_DATA } from '../data/mocks';
 import { Http } from '@angular/http';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import { User } from '../model/user';
 import { AuthService } from './auth.service';
@@ -18,8 +18,15 @@ export class DataService {
             
     }
     getApiData(){
-       return this.httpClient
-        .get<User[]>("https://fir-soc-gen.firebaseio.com/userdata.json?auth="+this.authService.getToken() );
+    //    return this.httpClient
+    //     .get<User[]>("https://fir-soc-gen.firebaseio.com/userdata.json?auth="+this.authService.getToken() );
+
+    // return this.httpClient
+    //     .get<User[]>("https://fir-soc-gen.firebaseio.com/userdata.json",{
+    //         params : new HttpParams().set("auth", this.authService.getToken())
+    //     });
+    return this.httpClient
+       .get<User[]>("https://fir-soc-gen.firebaseio.com/userdata.json")
     }
     constructor(private http : Http,
                 private httpClient : HttpClient,
